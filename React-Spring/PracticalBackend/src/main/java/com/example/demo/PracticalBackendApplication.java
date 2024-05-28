d8415d7198fa81a77f38c6ec2eafe621c690d1f9
Vaibhav Jagannath Kadam <107555072+Vaibhavk9921@users.noreply.github.com>
2024-05-28 15:23:16 +0530

    Add files via upload

---
+++ React-Spring/PracticalBackend/src/main/java/com/example/demo/PracticalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalBackendApplication.class, args);
+	}
+
+}
